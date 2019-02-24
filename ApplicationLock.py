8fa7af1903f0e1e713204aa84e0ebb9c80847d76
Thatoneguyouknow <benjistaman.12@gmail.com>
2019-02-23 17:04:21 -0700

    Add files via upload
    Aight, here is my registry editor class with everything that's been done on it so far. I also edited the ApplicationLock file to create these registry values when you click Engage Lock.

--- ApplicationLock.py
+++ ApplicationLock.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import ttk
+import File_Test, time_test, reg_handler
 
 root=Tk()
 root.title("Application Lock")
@@ -12,6 +13,21 @@ mainframe.rowconfigure(0, weight=1)
 WindowToBlock=StringVar() 
 TimeDuration=StringVar() 
 
+
+def run_locker():
+  # Setupworks properly, however application blocking is incorrect in my registry
+  # Try to create a registry key that blocks an application whether it be under CurrentVersion\Explorer or
+  # under CurrentVersion\Policies
+  Editor = reg_handler.Registry_Editor()
+  Editor.registry_setup()
+  if WindowToBlock.get() == " " or WindowToBlock.get() == "":
+    print("No application was selected")
+  else:
+    print("The string is:", WindowToBlock.get(), ".")
+    #reg_handler.add_new_app(WindowToBlock.get(), "1")
+
+
+  
 def search():
   print("The requested app to block is " + WindowToBlock.get())
   print("The requested time to block  is " + TimeDuration.get() + " minutes")
@@ -25,7 +41,7 @@ ttk.Label(mainframe, text="How Long Would You Like To Block The Application").gr
 
 ttk.Entry(mainframe, width=7, textvariable=TimeDuration).grid(column=2, row=4)
 
-ttk.Button(mainframe, text="Engage Lock", command=search).grid(column=2, row=13)
+ttk.Button(mainframe, text="Engage Lock", command=run_locker).grid(column=2, row=13)
 
 
 root.mainloop()
