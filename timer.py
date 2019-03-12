87357a22b8aa266f1285366365edd3950f88f334
Steven Enriquez <stevenenriquez123@gmail.com>
2019-03-12 10:10:10 -0600

    Added Functionality
    Included functionality to pass in a time unit as a parameter

--- timer.py
+++ timer.py
@@ -1,28 +1,50 @@
 import threading
 import time
 
-# Function: Timer (utilizes threading library)
-def run_timer( minutes, function ):
+# initialize unit variables
+SECOND = 'SEC'
+MINUTE = 'MIN'
+HOUR = 'HOUR'
 
+# Function: run_timer
+# Desc: Utilizes a threaded Timer Object
+#       Will run passed in function after the timer runs
+# Param: valid unit values: SECOND, MINUTE, HOUR
+def run_timer( duration, unit, function ):
+	
 	# calculate duration in seconds
-	seconds = minutes * 60
+	if( unit == SECOND ):
+		seconds = duration
+	elif( unit == MINUTE ):
+		seconds = duration * 60
+	elif( unit == HOUR ):
+		seconds = duration * 3600
+	else:
+		print("Invalid Unit: Defaulting to SECONDS")
+		seconds = duration
 
 	# initialize timer object
 	timer = threading.Timer( seconds, function)
-	
+
 	# start timer for passed in duration (seconds)
 	timer.start()
-
 	# waiting for time completion
 	timer.join()
 
-# Function: Timer (utilizes time.sleep)
-def run_sleep_timer( minutes ):
-	
+# Function: run_sleep_timer
+# Desc: Utilizes time.sleep to run a timer
+# Param: valid unit values: SECOND, MINUTE, HOUR
+def run_sleep_timer( duration, unit ):
 	# calculate duration in seconds
-	seconds = minutes * 60
+	if( unit == SECOND ):
+		seconds = duration
+	elif( unit == MINUTE ):
+		seconds = duration * 60
+	elif( unit == HOUR ):
+		seconds = duration * 3600
+	else:
+		print("Invalid Unit: Defaulting to SECONDS")
+		seconds = duration
 
 	# run timer for passed in duration
 	time.sleep( seconds )
-
-
