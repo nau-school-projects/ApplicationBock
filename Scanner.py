473d63a2827afaf7c1434fb35c53a00727d8e7c0
jkaufy <47403477+jkaufy@users.noreply.github.com>
2019-03-11 20:24:55 -0600

    working Scanner.py

--- Scanner.py
+++ Scanner.py
@@ -1,29 +1,30 @@
 import urllib
 from urllib.error import URLError
 from urllib.error import HTTPError
-from urllib.request import urlopen#used to test if a website exsists
+from urllib.request import urlopen          #used to test if a website exists
+
+from os.path import exists                  # used to test if a file exists
 
 def addWebsite( url, websiteList ):
 
     try:
         connection = urlopen( url )
     except HTTPError:
-        print("Not Valid URL") #temporary print outs!
+        print("Not Valid URL1")
     except URLError:
-        print("Not Valid URL")
+        print("Not Valid URL2")
     except ValueError:
-        print("Not Valid URL")
+        print("Not Valid URL3")
     else:
         websiteList.append( url )
     
 
 def addApplication( filePath, applicationList ):
-    try:
-        fileAccess = filePath.resolve( strict=True )
-        
-    except FileNotFoundError:
-        print("File Not Found")#temporary print out
-        
-    else:
+    if exists( filePath ):
         applicationList.append( filePath )
+    else:
+        print("file not found")
+
+
+
 
