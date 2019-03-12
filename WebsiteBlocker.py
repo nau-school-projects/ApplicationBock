1da2738d8b72b4c9ee37d2e85b5154215fff6333
jkaufy <47403477+jkaufy@users.noreply.github.com>
2019-03-12 14:47:57 -0700

    complete WebsiteBlocker.py
    changed the parameter from a list to a dictionary

--- WebsiteBlocker.py
+++ WebsiteBlocker.py
@@ -1,32 +1,39 @@
-localHost = "127.0.0.1"  #defualt local host
-hostFile = r"C:\Windows\System32\Drivers\etc\hosts" #default file path for windows host file
+LOCALHOST = "127.0.0.1"  #defualt local host
+HOSTFILE = r"C:\Windows\System32\Drivers\etc\hosts" #default file path for windows host file
 
-    
-def blockWebsite( blockedList ):
-    with open( hostFile, 'r+') as hostFileOpen:
+
+def blockWebsite( blockedDict ):
+    blocked = True
+    with open( HOSTFILE, 'r+') as hostFileOpen:
         hostFileContent = hostFileOpen.read()
 
-        for website in blockedList:
-            if website in hostFileContent:
+        for key in blockedDict:
+            if key in hostFileContent:
                 pass
+            
             else:
-                hostFileOpen.write( localHost + " " + website + "\n" )
+                hostFileOpen.write( LOCALHOST + " " + key + "\n" )
+                
+                blockedDict[key] = blocked
 
-def unblockWebsite( blockedList):
-    with open( hostFile, 'r+') as hostFileOpen:
+    return blockedDict
+
+def unblockWebsite( blockedDict):
+    blocked = False
+    with open( HOSTFILE, 'r+') as hostFileOpen:
        
         hostFileContent = hostFileOpen.readlines()
 
         hostFileOpen.seek(0)
 
         for line in hostFileContent:
-            for website in blockedList:
-                if not website in line:
+            for key in blockedDict:
+                if not key in line:
                     hostFileOpen.write(line)
+                else:
+                    blockedDict[key] = False
                     
         hostFileOpen.truncate()
         
-# must be ran as administrator
-
-
-    
+        return blockedDict
+# must be ran as administrator 
