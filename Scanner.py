e510c06c3dcdbdf2f896a9ab38ed1d2f5868c090
Steven Enriquez <stevenenriquez123@gmail.com>
2019-04-16 08:34:59 -0700

    Cleaned up exceptions, fixed typos
    Merged the HTTP, URL, and Value exceptions into one parenthesized tuple, and fixed typos in the comments

--- Scanner.py
+++ Scanner.py
@@ -1,8 +1,8 @@
 from urllib.error import URLError
 from urllib.error import HTTPError
-from urllib.request import urlopen #used to test if a website exsists
+from urllib.request import urlopen # used to test if a website exists
 
-BLOCKED = False      # sets to websites to not blocked when added to the list
+BLOCKED = False      # sets websites to not blocked when added to the list
 EXECUTABLE = "exe"   # used to check if the file is an executible
 
 
@@ -10,7 +10,7 @@ class Scanner:       # creates Scanner class
 
     # takes in a websites hostname and creates a full
     # URL and then adds the website to the blocked list
-    # if the website exsists
+    # if the website exists
     def addWebsite( hostName, websiteDict ):
         
     url = "https://" + hostName           # creates the url from the hostname 
@@ -18,14 +18,8 @@ class Scanner:       # creates Scanner class
     try:
         connection = urlopen( url )       # opens a connection with the websites and grabs the response
         
-    except HTTPError:                     # checks for a HTTP error 
-        print("Not Valid WEBSITE")
-        
-    except URLError:                      # checks for a URL error
-        print("Not Valid WEBSITE")
-        
-    except ValueError:                    # checks for a value error
-        print("Not Valid WEBSITE")
+    except (HTTPError, URLError, ValueError) as error:
+        print("Not a Valid Website")      # checks for HTTP, URL, and Value errors
         
     else:                                 # if there are no errors 
         websiteDict[hostName] = BLOCKED   # adds the blocked dictionary
@@ -35,9 +29,9 @@ class Scanner:       # creates Scanner class
     # to the application dictionary if the its and
     # executible 
     def addApplication( filePath, applicationDict ):
-        if filePath[-3:] == EXECUTABLE:                            # if the file name is an executible
-            applicationDict[filePath] = BLOCKED    # add the file name to the blocked list
+        if filePath[-3:] == EXECUTABLE:                    # if the file name is an executible
+            applicationDict[filePath] = BLOCKED            # add the file name to the blocked list
         else:
-             print("File not an executable")                        # otherwise its not an executible
+             print("File not an executable")               # otherwise its not an executible
 
 
