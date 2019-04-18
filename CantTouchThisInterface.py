6312af9031701aa92acddc938306fca173ab30cb
Benjamin-Couey <47403497+Benjamin-Couey@users.noreply.github.com>
2019-04-18 13:35:19 -0700

    Refactored the interface into a class
    Allows the interface to switch between multiple windows, such as a log in window and the main interface

--- CantTouchThisInterface.py
+++ CantTouchThisInterface.py
@@ -1,178 +1,242 @@
 from tkinter import *
 from tkinter import ttk
 from BlockedList import *
-#from timer import *
-#from Scanner import *
+from UserManager import *
 from timer import *
+#from Scanner import *
 
 import time
 import threading
 
-# INITIALIZATION
+class MainWindow:
 
-# initialize BlockedList
-# NOTE: in the future, here we need to ask the user to log in, read from
-# our file to get their unique BlockedList, and initialize it that way.
-blockList = BlockedList()
+  # THREAD
 
-# initialize window
-root=Tk()
-root.title("Can't Touch This")
-mainframe = ttk.Frame(root, padding="10 10 12 12")
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-mainframe.columnconfigure(0, weight=1)
-mainframe.rowconfigure(0, weight=1)
-mainframe.pack(fill=None, expand=False)
+  class timerThread( threading.Thread ):
+    def __init__( self, hours, minutes, timerLabel ):
+      threading.Thread.__init__(self)
+      self.hours = hours
+      self.minutes = minutes
+      self.timerLabel = timerLabel
 
-# initialize variables
-appToBlock = StringVar()
-websiteToBlock = StringVar()
-pinNumber = StringVar()
-blockLevel = StringVar()
-# NOTE: Need way to differentiate units user enters
-# ideally, user doens't have to type anything and can select number of hours / minutes / seconds
-# might not even need seconds, except for teting purposes
-numMinutes=IntVar()
-numHours=IntVar()
-timerLabel = None
-fullList = None
+    def run(self):
+      # initalizing variables
+      hours = self.hours
+      minutes = self.minutes;
+      seconds = 0
+      timerLabel = self.timerLabel
 
+      # calculate total duration in seconds
+      duration = int(( hours * 3600 ) + ( minutes * 60 ))
 
-# THREAD
+      # iterate through duration ( 0 to duration )
+      for i in range(duration):
+        if seconds == 0:
+          if minutes != 0:
+            minutes = minutes - 1
+            seconds = 60
+        if minutes == 0:
+          if hours != 0:
+            hours = hours - 1
+            minutes = 59
+            seconds = 60
 
-class timerThread( threading.Thread ):
-  def __init__( self, hours, minutes, timerLabel ):
-    threading.Thread.__init__(self)
-    self.hours = hours
-    self.minutes = minutes
-    self.timerLabel = timerLabel
+        # decremenet seconds
+        seconds = seconds - 1
 
-  def run(self):
-    # initalizing variables
-    hours = self.hours
-    minutes = self.minutes;
-    seconds = 0
-    timerLabel = self.timerLabel
+        # wait for 1 second
+        time.sleep(1)
 
-    # calculate total duration in seconds
-    duration = int(( hours * 3600 ) + ( minutes * 60 ))
+        # output current timer countdown
+        currentList = timerLabel[ "text" ]
+        currentList = ( hours, ":", minutes, ":", seconds )
+        timerLabel[ "text" ] = currentList
 
-    # iterate through duration ( 0 to duration )
-    for i in range(duration):
-      if seconds == 0:
-        if minutes != 0:
-          minutes = minutes - 1
-          seconds = 60
-      if minutes == 0:
-        if hours != 0:
-          hours = hours - 1
-          minutes = 59
-          seconds = 60
-
-      # decremenet seconds
-      seconds = seconds - 1
-
-      # wait for 1 second
-      time.sleep(1)
-
-      # output current timer countdown
-      currentList = timerLabel[ "text" ]
-      currentList = ( hours, ":", minutes, ":", seconds )
-      timerLabel[ "text" ] = currentList
-
-    # time is up, unblock everything
-    timerLabel[ "text" ] = ""
-    blockList.disallowApps( False )
-    blockList.unblockWebsite()
-
-# FUNCTIONS
-def search():
-  print("The requested app to block is " + appToBlock.get())
-  print("The requested time to block  is " + timeDuration.get() + " minutes")
-  print("Optional: create a Pin # for lock recent " + pinNumber.get() + " minutes")
-  print("The requested block level is " + blockLevel.get())
-  print("Number of hours to block the application " + numHours.get() + " hours")
-  print("Number of minutes to block the application" + numMinutes.get() + "hours")
-  return ''
-
-def addApp():
-  blockList.appDict[ appToBlock.get() ] = BLOCKED
+      # time is up, unblock everything
+      timerLabel[ "text" ] = ""
+      blockList.disallowApps( False )
+      blockList.unblockWebsite()
   
-  currentList = fullList[ "text" ]
-  currentList += ( appToBlock.get() + " - " + "BLOCKED\n" )
-  fullList[ "text" ] = currentList
 
-def addWebsite():
-  blockList.webDict[ websiteToBlock.get() ] = BLOCKED
+  def __init__( self, root, userManager ):
+    # initialize variables
+    
+    # initialize BlockedList
+    # NOTE: in the future, here we need to ask the user to log in, read from
+    # our file to get their unique BlockedList, and initialize it that way.
+    self.blockList = BlockedList()
+    self.appToBlock = StringVar()
+    self.websiteToBlock = StringVar()
+    self.blockLevel = StringVar()
+    # NOTE: Need way to differentiate units user enters
+    # ideally, user doens't have to type anything and can select number of hours / minutes / seconds
+    # might not even need seconds, except for teting purposes
+    self.numMinutes=IntVar()
+    self.numHours=IntVar()
+    self.timerLabel = None
+    self.fullList = None    
 
-  currentList = fullList[ "text" ]
-  currentList += ( websiteToBlock.get() + " - " + "BLOCKED\n" )
-  fullList[ "text" ] = currentList
+    # create window
+    self.root=root
+    self.root.title("Can't Touch This")
+    self.mainframe = ttk.Frame(self.root, padding="10 10 12 12")
+    self.mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+    self.mainframe.columnconfigure(0, weight=1)
+    self.mainframe.rowconfigure(0, weight=1)
+    self.mainframe.pack(fill=None, expand=False)
 
-def activateTimedBlock():
-    # applications
-    blockList.disallowApps( True )
-    #updateRegistry( blockList.appDict )
-    blockList.updateRegistry()
+    # WINDOW
 
-    # websites
-    blockList.blockWebsite()
+    # TIMER
+    self.timerLabel = ttk.Label(self.mainframe, text="")
+    self.timerLabel.grid(column=1, row=1)
 
-    # spool off thread to unblock after time is up
-    newThread = timerThread( numHours.get(), numMinutes.get(), timerLabel )
-    newThread.start()
+    # ADD APP TO BE BLOCKED
+    ttk.Label(self.mainframe, text="What Application Would You Like To Lock").grid(column=2, row=1)
+
+    ttk.Entry(self.mainframe, width=21, textvariable=self.appToBlock).grid(column=2, row=2)
+
+    # ADD APP BUTTON
+    ttk.Button(self.mainframe, text="Add", command=self.addApp).grid(column=3, row=2)
+
+    # ADD WEBSITE TO BE BLOCKED
+    ttk.Label(self.mainframe, text="What Website Would You Like To Lock").grid(column=2, row=3)
+
+    ttk.Entry(self.mainframe, width=21, textvariable=self.websiteToBlock).grid(column=2, row=4)
+
+    # ADD WEBSITE BUTTON
+    ttk.Button(self.mainframe, text="Add", command=self.addWebsite).grid(column=3, row=4)
+
+    # LIST OF APPS/WEBSITE
+    self.fullList = ttk.Label(self.mainframe, text="")
+    #fullList = Text(mainframe, height=10, width=30, state=DISABLED)
+    self.fullList.grid(column=4, row=1, rowspan = 10, sticky = N+S)
+
+    ## TIME TO BE BLOCKED FOR
+    ttk.Label(self.mainframe, text="How Long Would You Like To Block The Application").grid(column=2, row=5)
+
+    ttk.Label(self.mainframe, text="Enter Hours").grid(column=2, row=6)
+    ttk.Entry(self.mainframe, width=5, textvariable=self.numHours).grid(column=2, row=7)
+
+    ttk.Label(self.mainframe, text="Enter Minutes").grid(column=2, row=8)
+    ttk.Entry(self.mainframe, width=5, textvariable=self.numMinutes).grid(column=2, row=9)
+
+    # BLOCK LEVEL 1-2
+    #ttk.Label(mainframe, text="Enter Block Level 1 = Minor Block, 2 = Tedious").grid(column=2, row=7)
+
+    #ttk.Entry(mainframe, width=14, textvariable=blockLevel).grid(column=2, row=8)
+
+    #BUTTON
+    ttk.Button(self.mainframe, text="Engage Lock", command=self.activateTimedBlock).grid(column=2, row=13)
+
+  # FUNCTIONS
+  def search( self ):
+    print("The requested app to block is " + appToBlock.get())
+    print("The requested time to block  is " + timeDuration.get() + " minutes")
+    print("Optional: create a Pin # for lock recent " + pinNumber.get() + " minutes")
+    print("The requested block level is " + blockLevel.get())
+    print("Number of hours to block the application " + numHours.get() + " hours")
+    print("Number of minutes to block the application" + numMinutes.get() + "hours")
+    return ''
+
+  def addApp( self ):
+    self.blockList.appDict[ self.appToBlock.get() ] = BLOCKED
+    
+    self.currentList = self.fullList[ "text" ]
+    self.currentList += ( self.appToBlock.get() + " - " + "BLOCKED\n" )
+    self.fullList[ "text" ] = self.currentList
+
+  def addWebsite( self ):
+    self.blockList.webDict[ self.websiteToBlock.get() ] = BLOCKED
+
+    self.currentList = self.fullList[ "text" ]
+    self.currentList += ( self.websiteToBlock.get() + " - " + "BLOCKED\n" )
+    self.fullList[ "text" ] = self.currentList
+
+  def activateTimedBlock( self ):
+      # applications
+      self.blockList.disallowApps( True )
+      #updateRegistry( blockList.appDict )
+      self.blockList.updateRegistry()
+
+      # websites
+      self.blockList.blockWebsite()
+
+      # spool off thread to unblock after time is up
+      newThread = self.timerThread( self.numHours.get(), self.numMinutes.get(), self.timerLabel )
+      newThread.start()
+
+
+class LoginWindow:
   
-# WINDOW
+  def __init__( self, root, userManager ):
+    # initialize variables
+    
+    # initialize BlockedList
+    # NOTE: in the future, here we need to ask the user to log in, read from
+    # our file to get their unique BlockedList, and initialize it that way.
+    self.userManager = userManager
+    self.username = StringVar()
+    self.password = StringVar()
+    self.message = None
+    
+    # create window
+    self.root=root
+    self.root.title("Can't Touch This")
+    self.mainframe = ttk.Frame(self.root, padding="10 10 12 12")
+    self.mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+    self.mainframe.columnconfigure(0, weight=1)
+    self.mainframe.rowconfigure(0, weight=1)
+    self.mainframe.pack(fill=None, expand=False)
 
-# TIMER
-timerLabel = ttk.Label(mainframe, text="")
-timerLabel.grid(column=1, row=1)
+    # WINDOW
 
-# ADD APP TO BE BLOCKED
-ttk.Label(mainframe, text="What Application Would You Like To Lock").grid(column=2, row=1)
+    # USERNAME
+    ttk.Label(self.mainframe, text="Username").grid(column=2, row=1)
 
-ttk.Entry(mainframe, width=21, textvariable=appToBlock).grid(column=2, row=2)
+    ttk.Entry(self.mainframe, width=21, textvariable=self.username).grid(column=2, row=2)
 
-# ADD APP BUTTON
-ttk.Button(mainframe, text="Add", command=addApp).grid(column=3, row=2)
+    # PASSWORD
+    ttk.Label(self.mainframe, text="Password").grid(column=2, row=3)
 
-# ADD WEBSITE TO BE BLOCKED
-ttk.Label(mainframe, text="What Website Would You Like To Lock").grid(column=2, row=3)
+    ttk.Entry(self.mainframe, width=21, show="*", textvariable=self.password).grid(column=2, row=4)
 
-ttk.Entry(mainframe, width=21, textvariable=websiteToBlock).grid(column=2, row=4)
+    # MESSAGE
+    self.message = ttk.Label(self.mainframe, text="").grid(column=2, row=5)
+    #self.message.grid(column=2, row=5)
 
-# ADD WEBSITE BUTTON
-ttk.Button(mainframe, text="Add", command=addWebsite).grid(column=3, row=4)
+    # LOGIN
+    ttk.Button(self.mainframe, text="Add", command=self.login).grid(column=2, row=6)
 
-# LIST OF APPS/WEBSITE
-fullList = ttk.Label(mainframe, text="")
-#fullList = Text(mainframe, height=10, width=30, state=DISABLED)
-fullList.grid(column=4, row=1, rowspan = 10, sticky = N+S)
+  def login( self ):
+    selectedProfile = self.userManager.checkPassword( self.username.get(), self.password.get() )
+    if selectedProfile != None:
+      MainWindow( self.root )
+    else:
+      self.message[ "text" ] = "username or password is incorrect"
 
-## TIME TO BE BLOCKED FOR
-ttk.Label(mainframe, text="How Long Would You Like To Block The Application").grid(column=2, row=5)
+def main():
 
-ttk.Label(mainframe, text="Enter Hours").grid(column=2, row=6)
-ttk.Entry(mainframe, width=5, textvariable=numHours).grid(column=2, row=7)
+    # here we initialzie the UserManager (this also could be done in a seperate Main class, but I dunno)
+    # read the file to set up the UserManager
+    # if there is no file/if the file is empty the UserManager should be empty and go straight to the main window
+    userManager = UserManager()
+  
+    root = Tk()
+    #app = MainWindow(root)
 
-ttk.Label(mainframe, text="Enter Minutes").grid(column=2, row=8)
-ttk.Entry(mainframe, width=5, textvariable=numMinutes).grid(column=2, row=9)
+    # check if the dictionary is empty - meaning that a file wasn't able to be
+    # loaded and there are no user profiles
+    # empty dictionaries will evaluate to false
+    if( userManager.usersDict ):
+      app = LoginWindow( root, userManager )
+    else:
+      app = MainWindow( root, userManager )
+      
+    root.mainloop()
 
-# APP PIN TO GET DISIRED APPS TO BLOCK
-#ttk.Label(mainframe, text="Enter a Pin").grid(column=2, row=5)
-
-#ttk.Entry(mainframe, width=14, textvariable=pinNumber).grid(column=2, row=6)
-
-# BLOCK LEVEL 1-2
-#ttk.Label(mainframe, text="Enter Block Level 1 = Minor Block, 2 = Tedious").grid(column=2, row=7)
-
-#ttk.Entry(mainframe, width=14, textvariable=blockLevel).grid(column=2, row=8)
-
-#BUTTON
-ttk.Button(mainframe, text="Engage Lock", command=activateTimedBlock).grid(column=2, row=13)
-
-
-root.mainloop()
+if __name__ == '__main__':
+    main()
 
 
 
