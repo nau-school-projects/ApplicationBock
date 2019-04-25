517e384d709a6407d7dacb2e8fe136cc47d46a12
Benjamin-Couey <47403497+Benjamin-Couey@users.noreply.github.com>
2019-04-25 13:07:44 -0700

    Reupload to merge with Jacob's work

--- BlockedListTesting.py
+++ BlockedListTesting.py
@@ -1,7 +1,8 @@
 import unittest
-from BlockedList import*
+from BlockedList import *
+import ctypes
 
-class BlockedListTestCases( unittest.TestCase ): # set up testing class
+class BlockedListTestCases( unittest.TestCase ):
 
     # sets up varaibles for every test
     def setUp( self ):
@@ -20,6 +21,38 @@ class BlockedListTestCases( unittest.TestCase ): # set up testing class
         test = BlockedList.unblockWebsite()
         self.assertEqual( test, 0)
 
-# runs all tests 
-def testBlockedList():
+    # Test that applicaiton blocker correctly modifies registries
+    def testApplicationBlocker( self ):
+        """Test case A. Tests BlockedList modifying registers"""
+
+        #assert sys.argv[-1] == ASADMIN, "BlockedList must be run as administrator to function"
+
+        # test to see if running as admin 
+        is_admin = ctypes.windll.shell32.IsUserAnAdmin() != 0
+        if not is_admin:
+            self.fail("BlockedList must be run as admin to function")
+        
+        blockListA = BlockedList()
+        blockListA.appDict["appA"] = BLOCKED
+        blockListA.appDict["appB"] = BLOCKED
+        blockListA.appDict["appC"] = BLOCKED
+        blockListA.appDict["appD"] = UNBLOCKED
+
+        blockListA.updateRegistry()
+
+        assert blockListA.keyIsPresent( "appA" ), "disallowRun registry not being correctly set"
+        assert blockListA.keyIsPresent( "appB" ), "disallowRun registry not being correctly set"
+        assert blockListA.keyIsPresent( "appC" ), "disallowRun registry not being correctly set"
+        assert blockListA.keyIsPresent( "appD" ) == False, "disallowRun registry not being correctly set"
+        
+        blockListA.disallowApps( True )
+
+        assert blockListA.appsAreBlocked(), "disallowApps not correctly modifying explorer registry"
+
+        blockListA.disallowApps( False )
+
+        assert blockListA.appsAreBlocked() == False, "disallowApps not correctly modifying explorer registry"
+
+# run all tests
+if __name__ == "__main__":
     unittest.main()
